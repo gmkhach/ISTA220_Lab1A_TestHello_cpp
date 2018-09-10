9b0637d9937feebf2b7fcc9c954d49aebd81f8fc
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-10 13:37:41 -0400

    basic implementation of Hello World using std

---
+++ TestHello_cpp/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!";
+
+	// cin.ignore() make the console to waint for any key to be pressed.
+	cin.ignore();
+
+	return 0;
+}
